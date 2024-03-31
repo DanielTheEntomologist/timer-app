eab54a60aaced38c9195471c93fed16e2f1271f5
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-03-31 23:26:50 +0200

    Style timer and create TimeDisplay component

--- src/components/TimeDisplay/TimeDisplay.js
+++ src/components/TimeDisplay/TimeDisplay.js
@@ -1,11 +1,24 @@
 import styles from "./TimeDisplay.module.scss";
 
-const TimeDisplay = ({ hours, minutes, seconds, miliseconds }) => {
-  return (
-    <div className={styles.timeDisplay}>
-      {hours}:{minutes}:{seconds}:{miliseconds}
-    </div>
-  );
+const TimeDisplay = ({ miliseconds }) => {
+  const formatTime = function (timeInMillis) {
+    timeInMillis = Math.floor(timeInMillis);
+    const hours = Math.floor(timeInMillis / 3600000);
+    const minutes = Math.floor((timeInMillis - hours * 3600000) / 60000);
+    const seconds = Math.floor(
+      (timeInMillis - hours * 3600000 - minutes * 60000) / 1000
+    );
+    const milliseconds =
+      timeInMillis - hours * 3600000 - minutes * 60000 - seconds * 1000;
+
+    return `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}:${milliseconds
+      .toString()
+      .padStart(3, "0")}`;
+  };
+
+  return <div className={styles.timeDisplay}>{formatTime(miliseconds)}</div>;
 };
 
 export default TimeDisplay;
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,16 +1,24 @@
 import TimeDisplay from "../../components/TimeDisplay/TimeDisplay.js";
 import Button from "../../components/Button/Button";
 
-// import React from "react";
-import styles from "../../styles/global.scss";
+import styles1 from "../../styles/global.scss";
+import styles from "./Timer.module.scss";
+
+import { useState, useEffect } from "react";
 
 const Timer = ({ children, onClick }) => {
+  const [timeDisplayed, setTimeDisplayed] = useState(0);
+
   return (
-    <div className={(styles.container, styles.timer)}>
-      <TimeDisplay hours="00" minutes="00" seconds="00" miliseconds="00" />
-      <Button>Start</Button>
-      <Button>Stop</Button>
-      <Button>Reset</Button>
+    <div className={(styles1.container, styles.timer)}>
+      <TimeDisplay miliseconds={timeDisplayed} />
+      {/* <div className={styles.timeDisplay} miliseconds={time}> */}
+
+      <div className={styles.controls}>
+        <Button>Start</Button>
+        <Button>Stop</Button>
+        <Button>Reset</Button>
+      </div>
     </div>
   );
 };
